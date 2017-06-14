36f091b0249b2a95dede01494a0e3b501989e9db
buurz <buurzx@gmail.com>
2017-06-14 23:51:16 +0300

    Init

---
+++ lib/thy_worker.ex
@@ -0,0 +1,15 @@
+defmodule ThyWorker do
+  def start_link do
+    spawn(fn -> loop end)
+  end
+
+  defp loop do
+    receive do
+      :stop -> :ok
+
+      msg ->
+        IO.inspect msg
+        loop
+    end
+  end
+end
